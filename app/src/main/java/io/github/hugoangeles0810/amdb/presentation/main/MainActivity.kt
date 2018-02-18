97be3db65cad45bb1f820e3b92e1d2869a2c5972
Hugo Angeles <hugoangeles0810@gmail.com>
2018-02-17 19:09:09 -0500

    Adding more dependencies

---
+++ app/src/main/java/io/github/hugoangeles0810/amdb/presentation/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.github.hugoangeles0810.amdb.presentation.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.hugoangeles0810.amdb.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
